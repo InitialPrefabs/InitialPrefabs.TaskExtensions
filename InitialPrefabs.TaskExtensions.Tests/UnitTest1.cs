7c20ce609cc3cc7e04e01acde2279f9e61bf11be
Porrith Suong <porrith.suong@gmail.com>
2025-02-01 15:50:30 -0800

    Working test setup using .net8.

---
+++ InitialPrefabs.TaskExtensions.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace InitialPrefabs.TaskExtensions.Tests
+{
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Class1.Add(1, 1, out var c);
+            Assert.That(c == 2);
+        }
+    }
+}

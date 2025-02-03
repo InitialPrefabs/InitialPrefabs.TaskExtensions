a1bb26b814d9b5445bdce2169df3d9810af94ceb
Porrith Suong <porrith.suong@gmail.com>
2025-02-03 12:40:33 -0800

    Adds a test for parallel adding.

--- InitialPrefabs.TaskExtensions.Tests/UnitTest1.cs
+++ InitialPrefabs.TaskExtensions.Tests/UnitTest1.cs
@@ -1,16 +1,38 @@
 ﻿using NUnit.Framework;
+using System.Threading.Tasks;
 
 namespace InitialPrefabs.TaskExtensions.Tests {
 
-    public class Tests {
+    public class TaskParallelForTests {
+        struct ParllelAdd : ITaskParallelFor {
+            public int[] A;
+            public int[] B;
+
+            public void Execute(int index) {
+                A[index] = A[index] + B[index];
+            }
+        }
+
         [SetUp]
         public void Setup() {
         }
 
         [Test]
         public void Test1() {
-            Class1.Add(1, 1, out var c);
-            Assert.That(c == 2);
+            var a = new [] { 1, 2, 3, 4 };
+            var b = new [] { 1, 2, 3, 4 };
+
+            var t = new ParllelAdd {
+                A = a,
+                B = b
+            }.Schedule(4, 4);
+
+            t.Wait();
+
+            for (var i = 0; i < a.Length; i++) {
+                var e = a[i];
+                Assert.That(e == b[i] * 2);
+            }
         }
     }
 }
--- InitialPrefabs.TaskExtensions/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace InitialPrefabs.TaskExtensions {
-    public class Class1 {
-        public static void Add(int a, int b, out int c) {
-            c = a + b;
-        }
-    }
-}

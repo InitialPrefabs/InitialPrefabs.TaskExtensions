7a8604dada0f03ba02db53c45043f76ee8feca90
Porrith Suong <porrith.suong@gmail.com>
2025-04-30 17:12:57 -0400

    Started working on a linked list.

---
+++ InitialPrefabs.TaskFlow.Tests/Collections/LinkedListTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace InitialPrefabs.TaskFlow.Collections.Tests {
+
+    public class LinkedListTests {
+
+        private LinkedList<int> linkedList;
+
+        [SetUp]
+        public void SetUp() {
+            linkedList = new LinkedList<int>(5);
+            Assert.Multiple(() => {
+                Assert.That(linkedList.Head, Is.EqualTo(-1));
+                Assert.That(linkedList.Tail, Is.EqualTo(-1));
+            });
+        }
+
+        private void CommonFirstTest(short head, short tail, short expected) {
+            var first = linkedList.Nodes[0];
+            Assert.Multiple(() => {
+                Assert.That(linkedList.Nodes[0], Is.Not.EqualTo(default));
+                Assert.That(first, Is.EqualTo(new LinkedList<int>.Node<int> {
+                    Next = 4,
+                    Previous = -1,
+                    Value = expected
+                }));
+
+                Assert.That(
+                    linkedList,
+                    Has.Count.EqualTo(1),
+                    "Nothing added to linked list.");
+
+                Assert.That(linkedList.Head, Is.EqualTo(head));
+                Assert.That(linkedList.Tail, Is.EqualTo(tail));
+            });
+        }
+
+        [Test]
+        public void AddToHeadTest() {
+            linkedList.AddFirst(10);
+            CommonFirstTest(0, 0, 10);
+        }
+
+        [Test]
+        public void AppendTest() {
+            linkedList.Append(11);
+            CommonFirstTest(0, 0, 11);
+            linkedList.Append(12);
+            Assert.Multiple(() => {
+                Assert.That(linkedList, Has.Count.EqualTo(2),
+                    "Tail not added.");
+                Assert.That(linkedList.Tail, Is.EqualTo(4),
+                    "The tail should have been updated.");
+            });
+        }
+    }
+}
+
---
+++ InitialPrefabs.TaskFlow/Collections/LinkedList.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace InitialPrefabs.TaskFlow.Collections {
+
+    public class LinkedList<T0> where T0 : unmanaged, IEquatable<T0> {
+
+        public struct Node<T1> : IEquatable<Node<T1>> where T1 : unmanaged, IEquatable<T1> {
+            public short Previous;
+            public short Next;
+            public T1 Value;
+
+            public readonly bool Equals(Node<T1> other) {
+                return other.Previous == Previous && other.Next == Next && other.Value.Equals(Value);
+            }
+        }
+
+        internal Node<T0>[] Nodes;
+        internal DynamicArray<short> FreeList;
+        internal DynamicArray<short> UseList;
+
+        public int Count => UseList.Count;
+
+        internal short Head;
+        internal short Tail;
+
+        public LinkedList(int capacity) {
+            Nodes = new Node<T0>[capacity];
+            FreeList = new DynamicArray<short>(capacity);
+            UseList = new DynamicArray<short>(capacity);
+
+            for (short i = 0; i < capacity; i++) {
+                FreeList.Add(i);
+            }
+
+            Head = -1;
+            Tail = -1;
+        }
+
+        public LinkedList(int capacity, T0 value) : this(capacity) {
+            AddFirst(value);
+        }
+
+        public void AddFirst(T0 item) {
+            // Take the first node
+            var freeIndex = FreeList[0];
+            FreeList.RemoveAtSwapback(0);
+            UseList.Add(freeIndex);
+
+            Nodes[freeIndex] = new Node<T0> {
+                Previous = -1,
+                Value = item,
+                Next = FreeList[0]
+            };
+
+            Head = freeIndex;
+            Tail = freeIndex;
+        }
+
+        public void Append(T0 item) {
+            if (UseList.Count == 0) {
+                AddFirst(item);
+            } else {
+                var last = UseList[^1];
+                var lastNode = Nodes[last];
+                var nextFree = lastNode.Next;
+
+                var idx = FreeList.Find(element => element == nextFree);
+#if DEBUG
+                if (idx == -1) {
+                    throw new InvalidOperationException();
+                }
+#endif
+
+                FreeList.RemoveAtSwapback(idx);
+                UseList.Add(nextFree);
+                var next = FreeList[0];
+                var node = new Node<T0> {
+                    Next = next,
+                    Value = item,
+                    Previous = last
+                };
+                Nodes[nextFree] = node;
+                Tail = nextFree;
+            }
+        }
+    }
+}
+

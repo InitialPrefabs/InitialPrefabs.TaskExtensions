7645be6e1d319c5fd39772bf7d2160820cc43a25
Porrith Suong <porrith.suong@gmail.com>
2025-05-04 16:51:18 -0400

    Removes the basic demo code.

--- InitialPrefabs.TaskFlow.Examples/Program.cs
+++ InitialPrefabs.TaskFlow.Examples/Program.cs
@@ -20,26 +20,6 @@ namespace InitialPrefabs.TaskFlow.Examples {
 
     public class Program {
         public static void Main(string[] argv) {
-            TaskHandleExtensions.Initialize(5);
-
-            var frame = 0;
-            var a = new int[100];
-            for (var i = 0; i < 100; i++) {
-                TaskHandleExtensions.Graph.Reset();
-                TaskHandleExtensions.Reset();
-
-                var handleA = new ResetTask {
-                    A = a
-                }.Schedule(100);
-
-                var _ = new AddTask {
-                    A = a
-                }.ScheduleParallel(a.Length, 10, handleA);
-
-                TaskHandleExtensions.Graph.Sort();
-                TaskHandleExtensions.Graph.Process();
-                frame++;
-            }
         }
     }
 }

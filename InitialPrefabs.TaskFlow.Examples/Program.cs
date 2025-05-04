e4f2fd4b94b623dbd451dea3306870d5c318ad62
Porrith Suong <porrith.suong@gmail.com>
2025-05-04 17:04:05 -0400

    Adds the basic sample.

--- InitialPrefabs.TaskFlow.Examples/Program.cs
+++ InitialPrefabs.TaskFlow.Examples/Program.cs
@@ -3,7 +3,6 @@
 namespace InitialPrefabs.TaskFlow.Examples {
 
     internal struct ResetTask : ITaskFor {
-
         public int[] A;
 
         public readonly void Execute(int index) {
@@ -13,6 +12,7 @@ namespace InitialPrefabs.TaskFlow.Examples {
 
     internal struct AddTask : ITaskFor {
         public int[] A;
+
         public readonly void Execute(int index) {
             A[index] += index;
         }
@@ -20,6 +20,23 @@ namespace InitialPrefabs.TaskFlow.Examples {
 
     public class Program {
         public static void Main(string[] argv) {
+            var graph = TaskGraphManager.Initialize();
+            var source = new int[100];
+
+            for (var i = 0; i < 100; i++) {
+                var handle = new AddTask {
+                    A = source
+                }.ScheduleParallel(100, 20);
+
+                _ = new ResetTask {
+                    A = source
+                }.ScheduleParallel(100, 20, handle);
+
+                graph.Sort();
+                graph.Process();
+            }
+
+            TaskGraphManager.Shutdown();
         }
     }
 }

e9d391bf2720e6a10b84c6b2e1ad5f116ab326f6
Porrith Suong <porrith.suong@gmail.com>
2025-05-23 14:01:56 -0400

    Started working on minimal abstraction layers in opengl s o ican load a bunch of objects to display.

---
+++ InitialPrefabs.TaskFlow.Examples/BufferObject.cs
@@ -0,0 +1,38 @@
+﻿using Silk.NET.OpenGL;
+using System;
+
+namespace InitialPrefabs.TaskFlow.Examples {
+
+    public readonly struct BufferObject<T> : IDisposable where T : unmanaged {
+
+        private readonly uint handle;
+        private readonly BufferTargetARB bufferType;
+        private readonly GL gl;
+
+        public BufferObject(GL gl, Span<T> data, BufferTargetARB bufferType) {
+            this.gl = gl;
+            this.bufferType = bufferType;
+            handle = gl.GenBuffer();
+            Bind();
+
+            unsafe {
+                fixed (void* ptr = data) {
+                    gl.BufferData(
+                        bufferType,
+                        InteropUtils.SizeOf(data),
+                        ptr,
+                        BufferUsageARB.StaticDraw);
+                }
+            }
+        }
+
+        private readonly void Bind() {
+            gl.BindBuffer(bufferType, handle);
+        }
+
+        public readonly void Dispose() {
+            gl.DeleteBuffer(handle);
+        }
+    }
+}
+
---
+++ InitialPrefabs.TaskFlow.Examples/InteropUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace InitialPrefabs.TaskFlow.Examples {
+    public static class InteropUtils {
+        public static nuint SizeOf<T>(Span<T> span) where T : unmanaged {
+            return (nuint)(span.Length * Marshal.SizeOf<T>());
+        }
+    }
+}
+
+
